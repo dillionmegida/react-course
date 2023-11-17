f97121dab1524fece0825ec8c5e69e3d5568bc01
Dillion Megida <dillionmegida@gmail.com>
2023-11-17 16:01:25 +0100

    improve project with rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,70 @@
 import PricingCard from "./components/PricingCard/PricingCard"
-import styles from './App.module.css'
+import styles from "./App.module.css"
+
+// const arr = [1,2,3,4]
+// const newArr = arr.filter(num => num !== 1)
+// // [2,3,4]
 
 function App() {
+  const cards = [
+    {
+      label: "Start-Up",
+      price: "Free",
+      image: "/images/bike.png",
+      imageAlt: "Moving bicycle with clouds",
+      benefits: [
+        "Unlimited Downloads",
+        "Email Support",
+        "Limited Access",
+      ],
+    },
+    {
+      label: "Pro",
+      price: "49$",
+      duration: "/Year",
+      image: "/images/car.png",
+      imageAlt: "Moving car with clouds",
+      benefits: [
+        "Upto 10 Users",
+        "Email Support, Call Support",
+        "1 Year Access",
+      ],
+    },
+    {
+      label: "Enterprise",
+      price: "99$",
+      image: "/images/plane.png",
+      imageAlt: "Flying plane with clouds",
+      benefits: [
+        "Unlimited Access",
+        "On demand request",
+        "Lifetime Access",
+      ],
+    },
+  ]
+
   return (
     <div className={styles.prices}>
-      <PricingCard
-        label="Start-Up"
-        price="Free"
-        image="/images/bike.png"
-        imageAlt="Moving bicycle with clouds"
-      />
-      <PricingCard
-        label="Pro"
-        price="49$"
-        duration="/Year"
-        image="/images/car.png"
-        imageAlt="Moving car with clouds"
-      />
-      <PricingCard
-        label="Enterprise"
-        price="99$"
-        image="/images/plane.png"
-        imageAlt="Flying plane with clouds"
-      />
+      {/* {[
+        { id: "hello", value: "Hello" },
+        { id: "hi", value: "Hi how are you" },
+        { id: "num1", value: 6 },
+        { id: "num2", value: 9 },
+      ].map(item => {
+        return <p key={item.id}>{item.value}</p>
+      })} */}
+      {cards.map(card => {
+        return (
+          <PricingCard
+            key={card.label}
+            label={card.label}
+            price={card.price}
+            image={card.image}
+            imageAlt={card.imageAlt}
+            benefits={card.benefits}
+          />
+        )
+      })}
     </div>
   )
 }
--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -6,6 +6,7 @@ export default function PricingCard({
   duration = "",
   image,
   imageAlt,
+  benefits
 }) {
   return (
     <div className={styles.card}>
@@ -15,43 +16,32 @@ export default function PricingCard({
           <img src={image} alt={imageAlt} />
         </div>
         <div className={styles["card__price-label"]}>
-            <span className={styles["card__price-figure"]}>{price}</span>
-            <span className={styles["card__price-duration"]}>{duration}</span>
+          <span className={styles["card__price-figure"]}>
+            {price}
+          </span>
+          <span className={styles["card__price-duration"]}>
+            {duration}
+          </span>
         </div>
         <span className={styles["card__benefits-label"]}>
           Everything in Free, Plus
         </span>
         <ul className={styles.card__benefits}>
-          <li
-            className={`${styles.card__benefit} center-horizontal`}
-          >
-            <img
-              src="/icons/check.png"
-              alt=""
-              className={styles["card__benefit-icon"]}
-            />
-            Upto 10 Users
-          </li>
-          <li
-            className={`${styles.card__benefit} center-horizontal`}
-          >
-            <img
-              src="/icons/check.png"
-              alt=""
-              className={styles["card__benefit-icon"]}
-            />
-            Email Support, Call Support
-          </li>
-          <li
-            className={`${styles.card__benefit} center-horizontal`}
-          >
-            <img
-              src="/icons/check.png"
-              alt=""
-              className={styles["card__benefit-icon"]}
-            />
-            1 Year Access
-          </li>
+          {benefits.map(benefit => {
+            return (
+              <li
+                key={benefit}
+                className={`${styles.card__benefit} center-horizontal`}
+              >
+                <img
+                  src="/icons/check.png"
+                  alt=""
+                  className={styles["card__benefit-icon"]}
+                />
+                {benefit}
+              </li>
+            )
+          })}
         </ul>
         <button className={styles.card__button}>Choose</button>
       </div>
