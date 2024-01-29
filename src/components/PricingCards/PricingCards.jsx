b04195092e9448493d3dd2d6a4ff23da9bf74bce
Dillion Megida <dillionmegida@gmail.com>
2024-01-29 10:52:41 +0100

    update code with lifecycle in functional components

--- src/components/PricingCards/PricingCards.jsx
+++ src/components/PricingCards/PricingCards.jsx
@@ -1,7 +1,7 @@
 import styles from "./PricingCards.module.css"
 import PropTypes from "prop-types"
 import PricingCard from "../PricingCard/PricingCard"
-import { useState, Component } from "react"
+import { useState, useEffect, useRef } from "react"
 
 const randomizeArr = array => {
   const cloned = [...array]
@@ -13,90 +13,114 @@ const randomizeArr = array => {
   return cloned
 }
 
-// function PricingCards({ cards }) {
-//   const [randomCards, setRandomCards] = useState(
-//     randomizeArr(cards)
-//   )
+function PricingCards({ cards }) {
+  const [randomCards, setRandomCards] = useState(
+    randomizeArr(cards)
+  )
 
-//   const randomizeCards = () => {
-//     setRandomCards(randomizeArr(randomCards))
+  const randomizeCards = () => {
+    setRandomCards(randomizeArr(randomCards))
+  }
+
+  const isMountedRef = useRef(false)
+
+  useEffect(() => {
+    if (isMountedRef.current) {
+      console.log("i am updated")
+    }
+  }, [randomCards])
+
+  useEffect(() => {
+    console.log("i am born")
+    isMountedRef.current = true
+
+    const intervalId = setInterval(() => {
+      console.log("hello hi hello")
+    }, 1000)
+
+    return () => {
+      console.log("cleaning up")
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  
+
+  return (
+    <div className={`${styles.prices} container`}>
+      <button
+        onClick={randomizeCards}
+        className={styles["randomize-btn"]}
+      >
+        ⚙
+      </button>
+      {randomCards.map(card => {
+        return <PricingCard key={card.label} card={card} />
+      })}
+    </div>
+  )
+}
+
+// class PricingCards extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       randomCards: randomizeArr(this.props.cards),
+//     }
 //   }
 
-//   return (
-//     <div className={`${styles.prices} container`}>
-//       <button
-//         onClick={randomizeCards}
-//         className={styles["randomize-btn"]}
-//       >
-//         ⚙
-//       </button>
-//       {randomCards.map(card => {
-//         return <PricingCard key={card.label} card={card} />
-//       })}
-//     </div>
-//   )
+//   intervalId = null
+
+//   randomizeCards = () => {
+//     this.setState({
+//       randomCards: randomizeArr(this.state.randomCards),
+//     })
+//   }
+
+//   componentDidMount () {
+//     const myButton = document.getElementById('my-button')
+//     // console.log(myButton)
+//     console.log('i am born')
+//     // this.intervalId = setInterval(() => {
+//     //   console.log('hello hi hello')
+//     // }, 1000)
+//     console.log(this)
+//     this.setState({})
+//   }
+
+//   shouldComponentUpdate = () => {
+//     // check state
+//     return true
+//   }
+
+//   componentDidUpdate = () => {
+//     console.log("hey i updated")
+//   }
+
+//   componentWillUnmount = () => {
+//     console.log('i am leaving')
+//     clearInterval(this.intervalId)
+//   }
+
+//   render() {
+//     const { randomCards } = this.state
+
+//     return (
+//       <div className={`${styles.prices} container`}>
+//         <button
+//         id='my-button'
+//           onClick={this.randomizeCards}
+//           className={styles["randomize-btn"]}
+//         >
+//           ⚙
+//         </button>
+//         {randomCards.map(card => {
+//           return <PricingCard key={card.label} card={card} />
+//         })}
+//       </div>
+//     )
+//   }
 // }
-
-class PricingCards extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      randomCards: randomizeArr(this.props.cards),
-    }
-  }
-
-  intervalId = null
-
-  randomizeCards = () => {
-    this.setState({
-      randomCards: randomizeArr(this.state.randomCards),
-    })
-  }
-
-  componentDidMount () {
-    const myButton = document.getElementById('my-button')
-    // console.log(myButton)
-    console.log('i am born')
-    // this.intervalId = setInterval(() => {
-    //   console.log('hello hi hello')
-    // }, 1000)
-    console.log(this)
-    this.setState({})
-  }
-
-  shouldComponentUpdate = () => {
-    // check state
-    return true
-  }
-
-  componentDidUpdate = () => {
-    console.log("hey i updated")
-  }
-
-  componentWillUnmount = () => {
-    console.log('i am leaving')
-    clearInterval(this.intervalId)
-  }
-
-  render() {
-    const { randomCards } = this.state
-
-    return (
-      <div className={`${styles.prices} container`}>
-        <button
-        id='my-button'
-          onClick={this.randomizeCards}
-          className={styles["randomize-btn"]}
-        >
-          ⚙
-        </button>
-        {randomCards.map(card => {
-          return <PricingCard key={card.label} card={card} />
-        })}
-      </div>
-    )
-  }
-}
 
 PricingCards.propTypes = {
   cards: PropTypes.arrayOf(
